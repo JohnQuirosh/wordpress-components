db33b50c17ab0bc76d8aa5c944ae11b46efcf482
John Quiros <jquirosh@Johns-MacBook-Pro.local>
2023-12-19 15:34:22 -0600

    add files

---
+++ tabs gutenberg/tabs-gutenberg.js
@@ -0,0 +1,12 @@
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+$(document).ready(function () {
+    $('.tab-content').hide();
+    $('.tab-content:first').show();
+    $('.tabs-nav .wp-block-button__link').click(function () {
+        let currentTab = $(this).attr('href');
+        $('.tab-content').hide();
+        $(currentTab).fadeIn();
+
+        return false;
+    });
+});
