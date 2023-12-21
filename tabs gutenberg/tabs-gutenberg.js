498fc9a034d7c570cc54628c0223b4901d0855a0
John Quiros <jquirosh@icloud.com>
2023-12-21 16:47:31 -0600

    file upload

--- tabs gutenberg/tabs-gutenberg.js
+++ tabs gutenberg/tabs-gutenberg.js
@@ -1,12 +1,17 @@
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-$(document).ready(function () {
-    $('.tab-content').hide();
-    $('.tab-content:first').show();
-    $('.tabs-nav .wp-block-button__link').click(function () {
+add_action('wp', function () { ?>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+        <script>
+    $(document).ready(function(){
+        $('.tab-content').hide();
+        $('.tab-content:first').show();
+      $('.tabs-nav .wp-block-button__link').click(function() {
         let currentTab = $(this).attr('href');
         $('.tab-content').hide();
         $(currentTab).fadeIn();
-
+    
         return false;
+      });
     });
-});
+    </script>
+    
+    <?php });
